d9e4d3790f357effb6749666c055284098269a28
JeanMarie Mariadassou <jmariadassou@icloud.com>
2021-01-13 07:27:17 -0800

    add login/logout

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
 
 from . import views
 
 app_name="home"
 urlpatterns = [
     path("", views.index, name="index"),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(),name='logout'),
 ]
 
--- prj/settings.py
+++ prj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,11 +53,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'prj.urls'
+# LOGIN_REDIRECT_URL = 'home'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'prj/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
