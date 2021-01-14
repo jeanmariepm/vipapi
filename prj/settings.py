d1425a385d302b7ee923b3f407f054762d0e1ab3
JeanMarie Mariadassou <jmariadassou@icloud.com>
2021-01-14 09:35:58 -0800

    Ready for heroku

--- prj/settings.py
+++ prj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'l7b^4=b_7@r@4ti#*n5u9e-=9(1*gl6qw*a%63e!l1e9omx9ud'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vipveed.herokuapp.com', 'localhost']
 
 
 # Application definition
