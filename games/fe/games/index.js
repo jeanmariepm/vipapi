91b5fcf977beee96973bc64a16c041a72b0ef59e
JeanMarie Mariadassou <jmariadassou@icloud.com>
2021-01-25 12:07:22 -0800

    Use React for games

---
+++ games/fe/games/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Hello = () => {
+  return (
+    <div>
+      <h1>Try these games</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<Hello/>, document.getElementById("App") )
